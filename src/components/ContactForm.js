e3fa5aa0bc3f20d39f7a15a49d96146b88fd6ca7
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-05-08 00:38:29 -0400

    tests passed, MVP

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const ContactForm = () => {
   const [data, setData] = useState();
   const { register, errors, handleSubmit } = useForm({
-    mode: "onBlur",
+    mode: 'onBlur',
   });
   const onSubmit = (data) => {
     setData(data);
@@ -16,9 +16,13 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            // grabbing firstName id
+            id="firstName"
             name="firstName"
             placeholder="Edd"
+            // fix code, so that test pass
             ref={register({ required: true, maxLength: 3 })}
+            // ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -28,6 +32,8 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            // grabbing lastName id
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,7 +47,9 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          {/* <input name="email" ref={register({ required: true })} /> */}
+          {/* input ud for email */}
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
@@ -51,11 +59,15 @@ const ContactForm = () => {
           <textarea name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre
+            data-testid="dataInfo"
+            style={{ textAlign: 'left', color: 'white' }}
+          >
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        {/* <input type="submit" /> */}
+        <input type="submit" data-testid="submit" />
       </form>
     </div>
   );
---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+import { render, fireEvent, getByTestId } from '@testing-library/react';
+
+test('test to render', () => {
+  render(<ContactForm />);
+});
+
+test('contact form will submit', () => {
+  const { getByLabelText, getByTestId } = render(<ContactForm />);
+  const firstNameInput = getByLabelText(/First Name/i);
+  const lastNameInput = getByLabelText(/Last Name/i);
+  const emailInput = getByLabelText(/Email/i);
+  fireEvent.change(firstNameInput, {
+    target: { name: 'firstName', value: 'Tiffany' },
+  });
+  expect(firstNameInput.value).toBe('Tiffany');
+
+  fireEvent.change(lastNameInput, {
+    target: { name: 'lastName', value: 'Crosby' },
+  });
+  expect(lastNameInput.value).toBe('Crosby');
+
+  fireEvent.change(emailInput, {
+    target: { name: 'email', value: 'tiffanycrosby@nothing.com' },
+  });
+  expect(emailInput.value).toBe('tiffanycrosby@nothing.com');
+
+  const submitBtn = getByTestId('submit');
+  fireEvent.click(submitBtn);
+});
