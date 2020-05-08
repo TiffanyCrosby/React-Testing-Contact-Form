869c250faf771b7b6659247f8ed79d5e1f7ec731
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-05-07 23:16:36 -0400

    have tests completed and working. will work on fireEvents

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,38 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders App without crashing", () => {
+test('renders App without crashing', () => {
   render(<App />);
+});
+
+test('renders First Name', () => {
+  const { getByText } = render(<App />);
+  const firstName = getByText(/first name/i);
+  expect(firstName).toBeInTheDocument();
+});
+
+test('renders Last Name', () => {
+  const { getByText } = render(<App />);
+  const LastName = getByText(/last name/i);
+  expect(LastName).toBeInTheDocument();
+});
+
+test('renders Email', () => {
+  const { getByText } = render(<App />);
+  const email = getByText(/email/i);
+  expect(email).toBeInTheDocument();
+});
+
+test('renders Message', () => {
+  const { getByText } = render(<App />);
+  const message = getByText(/message/i);
+  expect(message).toBeInTheDocument();
+});
+
+test('Changing input values', () => {
+  const { getByText } = render(<App />);
+  const firstNameInput = getByText(/first name*/i);
+  fireEvent.change(firstNameInput, { target: { value: 'Tiffany' } });
+  expect(firstNameInput.value).toBe('Tiffany');
 });
